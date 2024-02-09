8390b40e75101b94631c4ee0643d2cd5552ebd7d
Saifullah Latki <116554950+Saif-dev1@users.noreply.github.com>
2024-02-09 20:25:16 +0500

    variables.py
    variables , data types and maths operations

---
+++ Basic Task/variables.py
@@ -0,0 +1,21 @@
+#printing the different data types
+
+a=12
+b=12.44
+c="hello python"
+d= True
+print("int    float   String   boolean ")
+print(a,b,c,d)
+
+#Arithmetic operation in python
+x=8
+y=3
+
+print("Addition= ",x+y)
+print("Subtraction= ",x-y)
+print("Multipication= ",x*y)
+print("Divition (float)= ",x/y)
+print("Division (floor)= ",x//y)
+print("Modulus= ",x%y)
+print("Exponent= ",x**y)
+
