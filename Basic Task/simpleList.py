fa6f9fdf7ea25caa2c8b5f79c681e2f394e9490d
Saifullah Latki <116554950+Saif-dev1@users.noreply.github.com>
2024-02-12 22:32:24 +0500

    simpleList.py
    simple list functions

---
+++ Basic Task/simpleList.py
@@ -0,0 +1,11 @@
+l=[1,2,3,4,5,6]
+l2=['ali','ahmed','waseem','asad']
+print(l)
+l.reverse()#reverse the list
+print(l)
+l.copy()# make copy of list
+print(l)
+l.extend(l2)# combine both lists
+print (l)
+l.append('azal') # add more elements
+print(l)
