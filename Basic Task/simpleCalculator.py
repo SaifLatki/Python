f8839ddc369e958ca95a0da1d43d98da154d351c
Saifullah Latki <116554950+Saif-dev1@users.noreply.github.com>
2024-02-12 22:17:32 +0500

    simpleCalculator.py
    simple input of two numbers to perform simple operation

---
+++ Basic Task/simpleCalculator.py
@@ -0,0 +1,7 @@
+
+num1=input ('Enter the First Number: ')
+num2=input ('Enter the Second Number: ')
+
+print ('Addition of numbers: ',float(num1)+float(num2))
+print ('Subtraction of numbers: ',float(num1)-float(num2))
+print('Division of numbers: ',float(num1)/float(num2))
