84106ca7b9df92ca51610230b3bceba0031f0bfa
Saifullah Latki <116554950+Saif-dev1@users.noreply.github.com>
2024-02-20 23:05:00 +0500

    splitAndJoin.py
    split and join functions in list

---
+++ Lists/splitAndJoin.py
@@ -0,0 +1,9 @@
+mesg='welcome to python programing language'
+names='Ali,Ahmed,Asad,Naeem,Waseem,Azal'
+friend_list=['Aslam','Ali','Ahmed','Hassan','AB','Sherry']
+print(list(mesg))#every character becomes separate and changed to list
+print(mesg.split())#every word becomes separate and changed to list
+print(names.split())#cahnged into list
+
+print('-'.join(friend_list))#list changed into the statement and joined
+print(''.join(names))
