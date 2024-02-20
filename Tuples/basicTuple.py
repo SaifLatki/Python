c2134b0f7b3ec6a0db1acae297daf2ab417fcf33
saif-dev1 <saifullahlatki338@gmail.com>
2024-02-20 23:32:59 +0500

    List and tuple

---
+++ Tuples/basicTuple.py
@@ -0,0 +1,8 @@
+
+#introduction to tuples ,these are kind of lists
+
+friends=['ali','ahmed','asad','wajid','jon','Eric','jack']
+friends_tuple=('ali','ahmed','asad','wajid','jon','Eric','jack')
+# gives the same result as lists
+print (friends[2:6])
+print(friends_tuple[2:6])
