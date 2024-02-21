a971c5decefcee5c53cd7b839f66d785d64be606
saif-dev1 <saifullahlatki338@gmail.com>
2024-02-21 21:51:56 +0500

    union and intersection of two sets

---
+++ Sets/simpleSet.py
@@ -0,0 +1,4 @@
+friends={'saif','ali','hassan','asad','ahmed'}
+my_friends={'wajid','arslan','sherry','amir','ali','saif','asad'}
+print("all friends: ",friends.union(my_friends))
+print("same friends: ",friends.intersection(my_friends))
