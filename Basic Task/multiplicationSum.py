ebc040ed71485d30dde5e8c9c349714627ea7f3c
saif-dev1 <saifullahlatki338@gmail.com>
2024-02-26 22:19:19 +0500

    multplication and sum of two numbers

---
+++ Basic Task/multiplicationSum.py
@@ -0,0 +1,10 @@
+num1=input("Enter the first Number: ")
+num2=input("Enter the second Number: ")
+
+product=int (num1) * int (num2)
+
+if(product<=1000):
+    print("Product is: ",product)
+else:
+    print("Addition is: ",int(num1)+ int (num2))
+    
