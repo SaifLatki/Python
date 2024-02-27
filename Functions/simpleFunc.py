df17b1d5ec57767a0dfccf51073f63f9d88d2914
Saif-dev1 <saifullahlatki338@gmail.com>
2024-02-27 21:39:21 +0500

    Simple function and and call the function in python

---
+++ Functions/simpleFunc.py
@@ -0,0 +1,8 @@
+def personal(name, age,color):
+    print(f"My name is {name}, I'm {age} and I like {color} color")
+
+
+name=input("Enter the name: ")
+age=input ("Enter your age: ")
+color=input ("Enter your favourite color: ")
+personal(name,age,color)
