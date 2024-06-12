4c627e42b124148833a9175c48a9d72c87e29945
Faisal Ali Khan <faisal@example.com>
2024-06-12 23:50:31 +0500

    simple program

---
+++ Basic Task/traficLight.py
@@ -0,0 +1,9 @@
+signal=input("Enter the light Blanking: ")
+if(signal=="green" or signal=="Green"):
+    print("Go")
+elif(signal=="red" or signal=="Red"):
+    print("Stop")
+elif(signal=="blue" or signal=="Blue"):
+    print("Look")
+else:
+    print ("No Signals")
---
+++ Strings/silicing.py
@@ -0,0 +1,4 @@
+str=input("Enter the String: ")
+print("Orginal String is: ",str)
+
+print("Reverse String is: ",str[::-1])
